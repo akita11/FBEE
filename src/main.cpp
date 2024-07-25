1e91a3a2d6e53add1dc214ccb9c2c915522a2681
Junichi Akita <akita@ifdl.jp>
2024-07-25 18:13:51 +0900

    set ODR

--- src/main.cpp
+++ src/main.cpp
@@ -29,39 +29,64 @@
 BMI270::BMI270 bmi270;
 Adafruit_BMP280 bmp(&Wire);
 
+#define X 320
+#define Y 240
+
+uint8_t ax[X], ay[X], az[X];
+uint16_t p = 0;
+
 void setup() {
-    M5.begin();
-    M5.Ex_I2C.begin();
-		// ToDo set data rate, filters
+	M5.begin();
+	M5.Ex_I2C.begin();
 
-    unsigned status = bmp.begin(BMP280_SENSOR_ADDR);
-    if (!status) {
-        Serial.println(
-            F("Could not find a valid BMP280 sensor, check wiring or "
-              "try a different address!"));
-        Serial.print("SensorID was: 0x");
-        Serial.println(bmp.sensorID(), 16);
-        while (1) delay(10);
-    }
+  unsigned status = bmp.begin(BMP280_SENSOR_ADDR);
+	// ToDo set data rate, filters
+	// ref: (library's init()), Acc=BMI270
+	// https://github.com/m5stack/M5Unit-IMU-Pro-Mini/blob/main/src/BMI270.cpp
+	// Output data rate (ODR) : 1600Hz (max)
+	// Bandwidth : 740Hz @ ODR=1600Hz
 
-    bmi270.init(I2C_NUM_0, BIM270_SENSOR_ADDR);
+	bmi2_sens_config config;
+	config.cfg.acc.odr = BMI2_ACC_ODR_1600HZ;
+	config.cfg.acc.range = BMI2_ACC_RANGE_2G;
+	config.cfg.aux.manual_en = BMI2_DISABLE;
+	bmi270_set_sensor_config(&config, 1, &bmi2);
+
+	if (!status) {
+		printf("Could not find a valid BMP280 sensor, SensorID = 02x\n", bmp.sensorID());
+		while (1) delay(10);
+  }
+	bmi270.init(I2C_NUM_0, BIM270_SENSOR_ADDR);
+}
+
+#define MAX_ACC 1.0
+
+uint16_t conv_acc(float x)
+{
+	uint16_t v;
+	v = (x + MAX_ACC) / (2 * MAX_ACC) * Y;
+	if (v < 0) v = 0;
+	else if (v > Y -1) v = Y - 1;
+	return(v);
 }
 
 void loop(void) {
-    // put your main code here, to run repeatedly:
-    float x, y, z;
-
-    if (bmi270.accelerationAvailable()) {
-        bmi270.readAcceleration(x, y, z);
-
-        Serial.print("accel: \t");
-        Serial.print(x);
-        Serial.print('\t');
-        Serial.print(y);
-        Serial.print('\t');
-        Serial.print(z);
-        Serial.println();
-    }
-
-    delay(500);
+	float x, y, z;
+	if (bmi270.accelerationAvailable()) {
+		bmi270.readAcceleration(&x, &y, &z);
+		printf("x/y/z = %f / %f / %f\n"), x, y, z;
+		ax[p] = (float)(x + MAX_ACC) / (2 * MAX_ACC) * Y;
+		ax[p] = conv_acc(x);
+		ay[p] = conv_acc(y);
+		az[p] = conv_acc(z);
+		for (uint16_t x = 0; x < X; x++){
+			uint32_t dx = (x + p) % X;
+			M5.Display.drawFastVLine(x, 0, Y);
+			M5.Lcd.drawPixel(x, ax[dx], BLUE);
+			M5.Lcd.drawPixel(x, ay[dx], RED);
+			M5.Lcd.drawPixel(x, az[dx], GREEN);
+		}
+		p = (p + 1) % X;
+  }
+  delay(100);
 }
